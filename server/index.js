768d26abe45ef35ea3f0fe790c8fb3418e6d5b6b
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-30 02:40:31 +0530

    Connected server to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
+const app = express();
+
+// Setting body parser for sending request properly
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+/* MONGOOS setup */
+const CONNECTION_URL =
+  'mongodb+srv://m_ankit2108:231208@cluster0.azukqa9.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(`did not connect ${error}`));
+
+mongoose.set('useFindAndModify', false);
