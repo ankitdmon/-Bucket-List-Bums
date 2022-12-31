ba7b9aef0445370db702bd91e30701f557cd92cd
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-31 18:00:08 +0530

    Added SVG background

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './App';
+import './index.css';
 
 // Adding redux
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
